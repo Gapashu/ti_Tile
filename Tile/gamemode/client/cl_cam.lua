7304f5598127eaedc8f4d12561c92d5d66ba0049
Gapashu <wijohnnywii@yahoo.com>
2014-01-11 21:51:42 -0500

    COMMIT

--- Tile/gamemode/client/cl_cam.lua
+++ Tile/gamemode/client/cl_cam.lua
@@ -1,4 +1,5 @@
 if SERVER then return end
+resource.AddFile("materials/tile/ui/icon_24.png")
 
 
 G = 0
@@ -26,7 +27,6 @@ function GM:PostDrawOpaqueRenderables()
 
 
 pl = LocalPlayer()
-traceRes = pl:GetEyeTrace()
 
 G = G + 0.001
 x = math.cos(G)*190
@@ -39,10 +39,14 @@ draw.RoundedBox( 6, -x*2, -y*2,100, 100, Color( 255, 255, 255, 150 ) )
 draw.RoundedBox( 6, x*2, y*2,100, 100, Color( 0, 160, 255, 150 ) )
 draw.RoundedBox( 6, -x, -y,100, 100, Color( 0, 160, 255, 150 ) )
 
-surface.SetTextColor( 250, 100, 0, 255 )
-surface.SetTextPos( -300, 00 ) 
-surface.SetFont("BoxWall")
-surface.DrawText( "Welcome to Tiles!" )
+--surface.SetTextColor( 250, 100, 0, 255 )
+--surface.SetTextPos( -300, 00 ) 
+--surface.SetFont("BoxWall")
+--surface.DrawText( "Welcome to Tiles!" )
+
+local tex = Material("materials/tile/ui/icon_24.png")
+surface.SetMaterial(tex)
+surface.DrawTexturedRect(0, 0, 100,100)
 
 
 
